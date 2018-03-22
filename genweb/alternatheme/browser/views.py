91fc506599ce7bbb978b4991767a6591bda418e9
Roberto Diaz <robertodiaz76@gmail.com>
2018-03-22 18:36:41 +0100

    removed recaptcha dependency

--- genweb/alternatheme/browser/views.py
+++ genweb/alternatheme/browser/views.py
@@ -17,7 +17,6 @@ from plone.portlets.interfaces import IPortletManager
 from plone.portlets.interfaces import IPortletManagerRenderer
 from plone.app.layout.globals.layout import LayoutPolicy
 from plone.i18n.normalizer.interfaces import IIDNormalizer
-from plone.formwidget.recaptcha.view import RecaptchaView, IRecaptchaInfo
 
 from Products.CMFPlone import utils
 from Products.CMFPlone.utils import safe_unicode
@@ -31,9 +30,6 @@ from Products.CMFPlone.browser.navtree import getNavigationRoot
 from Products.CMFPlone.browser.navigation import CatalogNavigationTabs
 from Products.CMFPlone.browser.navigation import get_id, get_view_url
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-from recaptcha.client.captcha import displayhtml
-from collective.recaptcha.view import RecaptchaView as CollectiveRecaptchaView
 
 from genweb.core.interfaces import IHomePage
 from genweb.core.utils import genweb_config, pref_lang
@@ -483,120 +479,6 @@ class gwLayoutPolicy(LayoutPolicy):
                 body_class += ' userrole-' + role.lower()
 
         return body_class
-
-
-class gwRecaptchaView(RecaptchaView, grok.View):
-    """ Override of the original plone.formwidget.recaptcha view to match style
-        and language options """
-    grok.context(Interface)
-    grok.name('recaptcha')
-    grok.require('zope2.Public')
-    grok.layer(IGenwebTheme)
-
-    def render(self):
-        pass
-
-    def image_tag(self):
-        lang = pref_lang()
-        options = {"ca": """
-                            <script type="text/javascript">
-                                var RecaptchaOptions = {
-                                        custom_translations : {
-                                                instructions_visual : "Escriu les dues paraules:",
-                                                instructions_audio : "Transcriu el que sentis:",
-                                                play_again : "Torna a escoltar l'\u00e0udio",
-                                                cant_hear_this : "Descarrega la pista en MP3",
-                                                visual_challenge : "Modalitat visual",
-                                                audio_challenge : "Modalitat auditiva",
-                                                refresh_btn : "Demana dues noves paraules",
-                                                help_btn : "Ajuda",
-                                                incorrect_try_again : "Incorrecte. Torna-ho a provar.",
-                                        },
-                                        lang : '%s',
-                                        theme : 'clean'
-                                    };
-                            </script>
-                            """ % lang,
-                       "es": """
-                            <script type="text/javascript">
-                                var RecaptchaOptions = {
-                                        lang : '%s',
-                                        theme : 'clean'
-                                };
-                            </script>
-                            """ % lang,
-                       "en": """
-                            <script type="text/javascript">
-                                var RecaptchaOptions = {
-                                        lang : '%s',
-                                        theme : 'clean'
-                                };
-                            </script>
-                            """ % lang
-        }
-
-        if not self.settings.public_key:
-            raise ValueError('No recaptcha public key configured. Go to path/to/site/@@recaptcha-settings to configure.')
-        use_ssl = self.request['SERVER_URL'].startswith('https://')
-        error = IRecaptchaInfo(self.request).error
-        return options.get(lang, '') + displayhtml(self.settings.public_key, use_ssl=use_ssl, error=error)
-
-
-class gwCollectiveRecaptchaView(CollectiveRecaptchaView, grok.View):
-    """ Override of the original collective.recaptcha view to match style
-        and language options """
-    grok.context(Interface)
-    grok.name('captcha')
-    grok.require('zope2.Public')
-    grok.layer(IGenwebTheme)
-
-    def render(self):
-        pass
-
-    def image_tag(self):
-        lang = pref_lang()
-        options = {"ca": """
-                        <script type="text/javascript">
-                            var RecaptchaOptions = {
-                                    custom_translations : {
-                                            instructions_visual : "Escriu les dues paraules:",
-                                            instructions_audio : "Transcriu el que sentis:",
-                                            play_again : "Torna a escoltar l'\u00e0udio",
-                                            cant_hear_this : "Descarrega la pista en MP3",
-                                            visual_challenge : "Modalitat visual",
-                                            audio_challenge : "Modalitat auditiva",
-                                            refresh_btn : "Demana dues noves paraules",
-                                            help_btn : "Ajuda",
-                                            incorrect_try_again : "Incorrecte. Torna-ho a provar.",
-                                    },
-                                    lang : '%s',
-                                    theme : 'clean'
-                                };
-                        </script>
-                        """ % lang,
-                   "es": """
-                        <script type="text/javascript">
-                            var RecaptchaOptions = {
-                                    lang : '%s',
-                                    theme : 'clean'
-                            };
-                        </script>
-                        """ % lang,
-                   "en": """
-                        <script type="text/javascript">
-                            var RecaptchaOptions = {
-                                    lang : '%s',
-                                    theme : 'clean'
-                            };
-                        </script>
-                        """ % lang
-        }
-
-        if not self.settings.public_key:
-            raise ValueError('No recaptcha public key configured. Go to path/to/site/@@recaptcha-settings to configure.')
-        use_ssl = self.request['SERVER_URL'].startswith('https://')
-        error = IRecaptchaInfo(self.request).error
-        return options.get(lang, '') + displayhtml(self.settings.public_key, use_ssl=use_ssl, error=error)
 
 
 class gwSendEventView(grok.View):
